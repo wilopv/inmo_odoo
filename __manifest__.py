f2f51e172d1caecbcee284795890e164ea901f5b
wilopv <wilbertlopv@gmail.com>
2025-10-01 23:55:29 +0200

    feat: crear vistas de lista, kanban, formulario y busqueda.

--- __manifest__.py
+++ __manifest__.py
@@ -18,9 +18,7 @@
     "depends": ["base"],
     # always loaded
     "data": [
-        # 'security/ir.model.access.csv',
-        "views/views.xml",
-        "views/templates.xml",
+        "views/inmueble_views.xml",
         "security/ir.model.access.csv",
     ],
     "license": "LGPL-3",
