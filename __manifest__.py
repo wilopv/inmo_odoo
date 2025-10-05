d15959491a6e5d1ddbcb25a8ba98a4bf954be24e
wilopv <wilbertlopv@gmail.com>
2025-10-05 13:16:14 +0200

    chore: Añadido ícono para el módulo

--- __manifest__.py
+++ __manifest__.py
@@ -9,14 +9,10 @@
         del registro catastral.
     """,
     "author": "wilopv",
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+    "icon": "/inmo_odoo/static/description/icon.png",
     "category": "Bienes raíces",
     "version": "0.1",
-    # any module necessary for this one to work correctly
     "depends": ["base", "calendar"],
-    # always loaded
     "data": [
         "data/event_type_visita_inmobiliaria.xml",
         "views/inmueble_wizard_views.xml",
@@ -26,7 +22,6 @@
         "security/ir.model.access.csv",
     ],
     "license": "LGPL-3",
-    # only loaded in demonstration mode
     "demo": [
         "demo/demo.xml",
     ],
